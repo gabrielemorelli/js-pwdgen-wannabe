4e6da207d99be9d38e263be77de3023e5d7599d5
MyUsernme <gabriele.morelli@live.it>
2022-01-19 14:40:54 +0100

    esercizio completo

---
+++ main.js
@@ -0,0 +1,5 @@
+let name = prompt('Inserisci il tuo nome');
+let cognome = prompt('Inserisci il tuo cognome');
+let colore = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('name').innerHTML = name + cognome + colore + "21"; 
